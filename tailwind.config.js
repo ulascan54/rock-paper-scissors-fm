aeb5de7294b1e474ecb33465e17c3b4d8c790374
ulascan54 <ulascandemirbag@gmail.com>
2021-09-25 22:54:29 +0300

    header design complate

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   purge: ['./content/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'Dark-Text': 'hsl(229, 25%, 31%)',
+        'Score-Text': 'hsl(229, 64%, 46%)',
+        'Header-Outline': 'hsl(217, 16%, 45%)'
+      }
+    },
   },
   variants: {
     extend: {},
