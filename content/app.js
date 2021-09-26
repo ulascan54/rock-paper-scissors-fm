0f70a600ca23ff3ca4d89f19fc0489b1a5f7cf77
ulascan54 <ulascandemirbag@gmail.com>
2021-09-26 17:36:30 +0300

    bütün uygulama responsive hale getirildi

--- content/app.js
+++ content/app.js
@@ -2,18 +2,22 @@ const score_p=document.getElementById('score')
 
 const modalBtn=document.getElementById('modal-button')
 const modal=document.getElementById('modal')
-const modalClose=document.getElementById('modalClose')
+const modalClose=document.querySelectorAll('.modalClose')
 
 // MODAL
 modalBtn.addEventListener('click',()=>{
     modal.classList.remove('animate__fadeOut')
     modal.classList.add('animate__fadeIn')
-    modal.classList.remove('hidden')
+    modal.classList.remove('none')
 })
-modalClose.addEventListener('click',()=>{
-    modal.classList.remove('animate__fadeIn')
-    modal.classList.add('animate__fadeOut')
-    setTimeout(() => {
-        modal.classList.add('hidden')
-    }, 800);
-})
+modalClose.forEach(el => {
+    el.addEventListener('click',()=>{
+        modal.classList.remove('animate__fadeIn')
+        modal.classList.add('animate__fadeOut')
+        setTimeout(() => {
+            modal.classList.add('none')
+        }, 800);
+    })
+    
+});
+
